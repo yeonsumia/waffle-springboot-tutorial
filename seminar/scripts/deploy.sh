449550d1006ace21c59813ce76003cedd09cb7e6
EC2 Default User <ec2-user@ip-172-31-13-74.ap-northeast-2.compute.internal>
2021-10-31 08:38:13 +0000

    add deploy.sh

--- seminar/scripts/deploy.sh
+++ seminar/scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd /
+cd waffle-rookies-19.5-springboot
 git pull
-yeonsumia
-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
-cd waffle-rookies-19.5-springboot/seminar/
+cd seminar
 ./gradlew bootJar
 java -jar -Dspring.profiles.active=prod build/libs/seminar-0.0.1-SNAPSHOT.jar
 
