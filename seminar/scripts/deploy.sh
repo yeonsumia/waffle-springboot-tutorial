909c144b2fb36a3d51f58208fa9f78c77f336d4c
EC2 Default User <ec2-user@ip-172-31-13-74.ap-northeast-2.compute.internal>
2021-11-02 03:18:29 +0000

    modified deploy.sh

--- seminar/scripts/deploy.sh
+++ seminar/scripts/deploy.sh
@@ -4,6 +4,5 @@ cd waffle-rookies-19.5-springboot
 git pull
 cd seminar
 ./gradlew bootJar
-java -jar -Dspring.profiles.active=prod build/libs/seminar-0.0.1-SNAPSHOT.jar
-
+nohup java -jar -Dspring.profiles.active=prod build/libs/seminar-0.0.1-SNAPSHOT.jar & > /dev/null
 exit 0
